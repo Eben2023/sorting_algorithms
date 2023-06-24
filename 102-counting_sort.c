d800168a532898655d0a3771e4b1c2b4f01f4b33
Eben2023 <ebenprec@yahoo.com>
2023-06-24 07:34:34 -0700

    update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,91 +1,69 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
-
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-int tmp;
-
-tmp = *a;
-*a = *b;
-*b = tmp;
-}
-
-/**
- * lomuto_partition - Order a subset of an array of integers according to
- *the Lomuto partition scheme (last element as pivot).
- * @array: The array of integers.
- * @size: The size of the array.
- * @left: The starting index of the subset to order.
- * @right: The ending index of the subset to order.
- *
- * Return: The final partition index.
- */
-int lomuto_partition(int *array, size_t size, int left, int right)
-{
-int pivot, i, j;
-
-pivot = array[right];
-i = left - 1;
-
-for (j = left; j < right; j++)
-{
-if (array[j] <= pivot)
-{
-i++;
-swap_ints(&array[i], &array[j]);
-print_array(array, size);
-}
-}
-
-swap_ints(&array[i + 1], &array[right]);
-print_array(array, size);
-
-return (i + 1);
-}
-
-/**
- * lomuto_sort - Implement the quicksort algorithm through recursion.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- * @left: The starting index of the array partition to order.
- * @right: The ending index of the array partition to order.
- *
- * Description: Uses the Lomuto partition scheme.
- */
-void lomuto_sort(int *array, size_t size, int left, int right)
-{
-int part;
-
-if (left < right)
-{
-part = lomuto_partition(array, size, left, right);
-lomuto_sort(array, size, left, part - 1);
-lomuto_sort(array, size, part + 1, right);
-}
-}
-
-/**
- * quick_sort - Sort an array of integers in ascending
- *  order using the quicksort algorithm.
+ * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
  *
- * Description: Uses the Lomuto partition scheme. Prints
- *  the array after each swap of two elements.
+ * Return: The maximum integer in the array.
  */
-void quick_sort(int *array, size_t size)
+int get_max(int *array, int size)
 {
+int max, i;
+
+for (max = array[0], i = 1; i < size; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
+
+return max;
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ * using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void counting_sort(int *array, size_t size)
+{
+int *count, *sorted, max, i;
+
 if (array == NULL || size < 2)
 return;
 
-lomuto_sort(array, size, 0, size - 1);
+sorted = malloc(sizeof(int) * size);
+if (sorted == NULL)
+return;
+
+max = get_max(array, size);
+count = calloc(max + 1, sizeof(int));
+if (count == NULL)
+{
+free(sorted);
+return;
+}
+
+for (i = 0; i < (int)size; i++)
+count[array[i]]++;
+
+for (i = 1; i <= max; i++)
+count[i] += count[i - 1];
+
+for (i = (int)size - 1; i >= 0; i--)
+{
+sorted[count[array[i]] - 1] = array[i];
+count[array[i]]--;
+}
+
+for (i = 0; i < (int)size; i++)
+array[i] = sorted[i];
+
+print_array(count, max + 1);
+
+free(sorted);
+free(count);
 }
