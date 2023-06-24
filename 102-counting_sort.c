462cc8eb2651eddb00d035708229724c77d8ca8b
Eben2023 <ebenprec@yahoo.com>
2023-06-24 07:36:49 -0700

    update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -17,12 +17,12 @@ if (array[i] > max)
 max = array[i];
 }
 
-return max;
+return (max);
 }
 
 /**
  * counting_sort - Sort an array of integers in ascending order
- * using the counting sort algorithm.
+ *                 using the counting sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
@@ -38,31 +38,30 @@ return;
 sorted = malloc(sizeof(int) * size);
 if (sorted == NULL)
 return;
-
 max = get_max(array, size);
-count = calloc(max + 1, sizeof(int));
+count = malloc(sizeof(int) * (max + 1));
 if (count == NULL)
 {
 free(sorted);
 return;
 }
 
+for (i = 0; i < (max + 1); i++)
+count[i] = 0;
 for (i = 0; i < (int)size; i++)
-count[array[i]]++;
-
-for (i = 1; i <= max; i++)
+count[array[i]] += 1;
+for (i = 0; i < (max + 1); i++)
 count[i] += count[i - 1];
+print_array(count, max + 1);
 
-for (i = (int)size - 1; i >= 0; i--)
+for (i = 0; i < (int)size; i++)
 {
 sorted[count[array[i]] - 1] = array[i];
-count[array[i]]--;
+count[array[i]] -= 1;
 }
 
 for (i = 0; i < (int)size; i++)
 array[i] = sorted[i];
-
-print_array(count, max + 1);
 
 free(sorted);
 free(count);
