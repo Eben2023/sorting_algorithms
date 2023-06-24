a7e928d7b11897e415469fd947319ac7d315dc98
Eben2023 <ebenprec@yahoo.com>
2023-06-24 08:40:45 -0700

    update

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,17 +1,17 @@
 #include "sort.h"
 
-int get_max(int *array, int size);
-void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+int max_val(int *array, int size);
+void func_radix(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
 
 /**
- * get_max - Get the maximum value in an array of integers.
+ * max_val - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
  *
  * Return: The maximum integer in the array.
  */
-int get_max(int *array, int size)
+int max_val(int *array, int size)
 {
 int max, i;
 
@@ -25,14 +25,14 @@ return (max);
 }
 
 /**
- * radix_counting_sort - Sort the significant digits of an array of integers
+ * func_radix - Sort the significant digits of an array of integers
  *                       in ascending order using the counting sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  * @sig: The significant digit to sort on.
  * @buff: A buffer to store the sorted array.
  */
-void radix_counting_sort(int *array, size_t size, int sig, int *buff)
+void func_radix(int *array, size_t size, int sig, int *buff)
 {
 int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 size_t i;
@@ -73,10 +73,10 @@ buff = malloc(sizeof(int) * size);
 if (buff == NULL)
 return;
 
-max = get_max(array, size);
+max = max_val(array, size);
 for (sig = 1; max / sig > 0; sig *= 10)
 {
-radix_counting_sort(array, size, sig, buff);
+func_radix(array, size, sig, buff);
 print_array(array, size);
 }
 
