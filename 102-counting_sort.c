2c43ebe0ec33722335e48dd0d008d7c859319531
Eben2023 <ebenprec@yahoo.com>
2023-06-24 07:40:23 -0700

    update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,23 +1,23 @@
 #include "sort.h"
 
 /**
- * get_max - Get the maximum value in an array of integers.
+ * max_int - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
  *
  * Return: The maximum integer in the array.
  */
-int get_max(int *array, int size)
+int max_int(int *array, int size)
 {
-int max, i;
+int max_int, k;
 
-for (max = array[0], i = 1; i < size; i++)
+for (max_int = array[0], k = 1; k < size; k++)
 {
-if (array[i] > max)
-max = array[i];
+if (array[k] > max_int)
+max_int = array[k];
 }
 
-return (max);
+return (max_int);
 }
 
 /**
@@ -30,7 +30,7 @@ return (max);
  */
 void counting_sort(int *array, size_t size)
 {
-int *count, *sorted, max, i;
+int *count, *sorted, max_int, k;
 
 if (array == NULL || size < 2)
 return;
@@ -38,30 +38,30 @@ return;
 sorted = malloc(sizeof(int) * size);
 if (sorted == NULL)
 return;
-max = get_max(array, size);
-count = malloc(sizeof(int) * (max + 1));
+max_int = max_int(array, size);
+count = malloc(sizeof(int) * (max_int + 1));
 if (count == NULL)
 {
 free(sorted);
 return;
 }
 
-for (i = 0; i < (max + 1); i++)
-count[i] = 0;
-for (i = 0; i < (int)size; i++)
-count[array[i]] += 1;
-for (i = 0; i < (max + 1); i++)
-count[i] += count[i - 1];
-print_array(count, max + 1);
+for (k = 0; k < (max_int + 1); k++)
+count[k] = 0;
+for (k = 0; k < (int)size; k++)
+count[array[k]] += 1;
+for (k = 0; k < (max_int + 1); k++)
+count[k] += count[k - 1];
+print_array(count, max_int + 1);
 
-for (i = 0; i < (int)size; i++)
+for (k = 0; k < (int)size; k++)
 {
-sorted[count[array[i]] - 1] = array[i];
-count[array[i]] -= 1;
+sorted[count[array[k]] - 1] = array[k];
+count[array[k]] -= 1;
 }
 
-for (i = 0; i < (int)size; i++)
-array[i] = sorted[i];
+for (k = 0; k < (int)size; k++)
+array[k] = sorted[k];
 
 free(sorted);
 free(count);
